ac0431071d8d6bfda44fe97dff885b12227bf077
FoxDE <1719475579@qq.com>
2024-03-06 20:47:34 +0800

    ann example read

--- Homeworks/2_image_warping/documents/ann_example/src/ann_example.cpp
+++ Homeworks/2_image_warping/documents/ann_example/src/ann_example.cpp
@@ -7,25 +7,28 @@
 
 using namespace Annoy;
 int main() {
-  int f = 40;
+  int f = 1;
   AnnoyIndex<int, float, Euclidean, Kiss32Random, AnnoyIndexSingleThreadedBuildPolicy> index(f); //  π”√≈∑ œæ‡¿Î
 
   // Randomly sample 1000 points in dimension f
-  for (int i = 0; i < 1000; ++i) {
+  for (int i = 0; i < 10; ++i) {
     std::vector<float> vec(f);
     for (int z = 0; z < f; ++z) {
       vec[z] = (float)rand() / RAND_MAX;
+      std::cout << vec[z] << "    ";
+      if (z == f - 1)
+          std::cout << std::endl;
     }
     index.add_item(i, vec.data());
   }
 
-  index.build(10); // 10 trees
+  index.build(3); // 10 trees
 
-  int item_index = 43;
+  int item_index = 0;
   std::vector<int> closest_items;
   std::vector<float> distances;
   // Find 10 nearest neighbors of the item at index 42
-  index.get_nns_by_item(item_index, 10, -1, &closest_items, &distances); 
+  index.get_nns_by_item(item_index, 3, -1, &closest_items, &distances); 
 
   for (int i = 0; i < closest_items.size(); ++i) {
     std::cout << "Found neighbor: " << closest_items[i] << " Distance: " << distances[i] << std::endl;
