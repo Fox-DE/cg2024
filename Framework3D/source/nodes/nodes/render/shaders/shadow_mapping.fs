e79037ac49ed2a8754000200d5281cd29444bf1a
FoxDE <1719475579@qq.com>
2024-04-11 16:44:59 +0800

    no progress

--- Framework3D/source/nodes/nodes/render/shaders/shadow_mapping.fs
+++ Framework3D/source/nodes/nodes/render/shaders/shadow_mapping.fs
@@ -6,6 +6,7 @@ in vec3 vertexPosition;
 layout(location = 0) out float shadow_map0;
 
 void main() {
-    vec4 clipPos = light_projection * light_view * (vec4(vertexPosition, 1.0));
+    vec4 clipPos =  light_projection * light_view *(vec4(vertexPosition, 1.0));
+    //
     shadow_map0 = (clipPos.z / clipPos.w);
 }
