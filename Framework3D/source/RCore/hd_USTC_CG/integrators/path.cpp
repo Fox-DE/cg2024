e2335ca9b1e2d15c54fe03e988f015b8fdbc816e
FoxDE <1719475579@qq.com>
2024-04-17 22:50:14 +0800

    Path Tracing Done

--- Framework3D/source/RCore/hd_USTC_CG/integrators/path.cpp
+++ Framework3D/source/RCore/hd_USTC_CG/integrators/path.cpp
@@ -22,10 +22,16 @@ GfVec3f PathIntegrator::EstimateOutGoingRadiance(
     const std::function<float()>& uniform_float,
     int recursion_depth)
 {
+    
     if (recursion_depth >= 50) {
         return {};
     }
-
+    /* float RR = uniform_float();
+    if (recursion_depth > 1 && RR > 0.6)
+    {
+        return {};
+    }*/
+    //Russian Roullete
     SurfaceInteraction si;
     if (!Intersect(ray, si)) {
         if (recursion_depth == 0) {
@@ -44,12 +50,39 @@ GfVec3f PathIntegrator::EstimateOutGoingRadiance(
         si.flipNormal();
         si.PrepareTransforms();
     }
-
+    
     GfVec3f color{ 0 };
     GfVec3f directLight = EstimateDirectLight(si, uniform_float);
 
     // HW7_TODO: Estimate global lighting here.
-    GfVec3f globalLight;
+    GfVec3f globalLight = {0,0,0};
+    
+    GfRay Ray_next;
+    auto normal = si.shadingNormal;
+
+    //pick a random dir
+    auto basis = constructONB(normal);
+    GfVec3f dir_next;
+    float theta = 2.0f * M_PI * uniform_float();
+    float eta = uniform_float();
+    float sqrteta = sqrtf(eta);
+    dir_next[0] = cosf(theta) * sqrteta;
+    dir_next[1] = sinf(theta) * sqrteta;
+    dir_next[2] = sqrtf(1.0f - eta);
+    float pdf = dir_next[2] / M_PI;
+    //transform to world coordinate
+    auto worldSampledDir = basis * dir_next;
+    worldSampledDir = worldSampledDir.GetNormalized();
+    
+    //BRDF
+    auto eval = si.Eval(-worldSampledDir);
+    Ray_next.SetPointAndDirection(si.position + 0.000001 * normal, worldSampledDir);
+    auto cosVal = GfDot(worldSampledDir, normal);
+    
+    globalLight = cosVal*EstimateOutGoingRadiance(Ray_next, uniform_float, recursion_depth + 1)/pdf;
+    globalLight[0] = globalLight[0] * eval[0];
+    globalLight[1] = globalLight[1] * eval[1];
+    globalLight[2] = globalLight[2] * eval[2];
 
     color = directLight + globalLight;
 
